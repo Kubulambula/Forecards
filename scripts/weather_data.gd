bb5e1c4defab631a35ff76373a59388afe928f4a
Daniel <daniel.nemec.0@gmail.com>
2021-10-02 13:33:55 +0200

    Revert "new card placement"
    This reverts commit 4c98c670cfd2a8ea11eb910849fc9d605b9adbf3.

--- scripts/weather_data.gd
+++ scripts/weather_data.gd
@@ -5,11 +5,7 @@ enum {RAIN, TORNADO}
 var map = null
 
 var card_container = null setget set_card_container
-<<<<<<< Updated upstream:scripts/weather_data.gd
 var cards = ["res://scenes/cards/card.tscn"]
-=======
-var cards = ["res://scenes/cards/card.tscn", "res://scenes/cards/card.tscn", "res://scenes/cards/card.tscn", "res://scenes/cards/card.tscn", "res://scenes/cards/card.tscn", "res://scenes/cards/card.tscn",]
->>>>>>> Stashed changes:scripts/singletons/weather_data.gd
 
 # New container annouced itself so give it some data
 func set_card_container(value):
